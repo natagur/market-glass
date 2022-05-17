76322a369fb01bde7824d50537182dfc6dff89ef
@natalia_gureva <gur.natalia.n@yandex.ru>
2022-05-17 21:26:23 +0500

    калькулятор js, правки

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,40 @@
 let element = [];
-element[1] = [3,4]; 
-$(document).on ('click', '.calculator__select_choice input', function(event){
+//стекло
+element[1] = ['4a',8,9,'11a'];
+element[3] = ['4a',8,9,'11a']; 
+element[4] = ['4a',8,9,10,'11a',12]; 
+element[5] = ['4a',8,9,10,'11a',12,16]; 
+element[6] = ['4a',8,9,10,'11a',12,14,16]; 
+element[7] = ['4a',8,9,10,'11a',12,14,16];
+element[10] = ['4a',8,9,10,'11a',12,14,16]; 
+element[11] = ['4a',8,9,10,'11a',12,14,16]; 
+element[12] = ['4a',8,9,10,'11a',12,14,16]; 
+element[13] = ['4a',8,9,10,'11a',12,14,16]; 
+element[14] = ['4a',8,9,10,'11a',12,14,16]; 
+element[15] = ['4a',8,9,10,'11a',12,14,16]; 
+element[16] = ['4a',8,9,10,'11a',12,14,16]; 
 
-  $('.calculator__block label').removeClass('invisible');
-  // console.log($(this).parent() .data('id'))
-  console.log($(this).parent() .find('label') .data('id'));
+//зеркало
+element[2] = ['4',3,5,6]; 
+element['4a'] = ['4',3,5,6,10,11,12,13,14,15,16]; 
+element[7] = ['4',3,5,6,10,11,12,13,14,15,16]; 
+element[8] = ['4',3,5,6,10,11,12,13,14,15,16]; 
+element[9] = ['4',3,5,6,10,11,12,13,14,15,16]; 
+element['11a'] = ['4',3,5,6,10,11,12,13,14,15,16]; 
+$(document).on ('click', '.calculator__block input', function(event){
+
+  $('.calculator__block label').parent().removeClass('invisible');
   $.each(element[$(this).parent() .find('label') .data('id')], function (i, e){
    
-    $('.calculator__block label[data-id="' + e + '"]').addClass('invisible');
-    console.log(e);
-  })
+    $('.calculator__block label[data-id="' + e + '"]').parent().addClass('invisible');
+    
+  });
+
+  $('.calculator__block').removeClass('invisible-bg');
+  for (let i = 0;i < $(this).parents('.calculator__block').index()+2;i++){
+    console.log( i );
+    $('.calculator__block').eq(i).addClass('invisible-bg');
+  }
   
   
   });
